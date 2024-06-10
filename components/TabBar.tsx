9f1a6241eda7fed17cba7e136fbf8718b5742215
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-06-10 21:32:58 +0100

    fear: add tab bar

---
+++ components/TabBar.tsx
@@ -0,0 +1,69 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+
+interface TabBarProps {
+  state: any;
+  descriptors: any;
+  navigation: any;
+}
+
+const TabBar: React.FC<TabBarProps> = ({ state, descriptors, navigation }) => {
+  return (
+    <View style={{ flexDirection: "row" }}>
+      {state.routes.map(
+        (
+          route: { key: string | number; name: string; params: any },
+          index: number
+        ) => {
+          const { options } = descriptors[route.key];
+          const label =
+            options.tabBarLabel !== undefined
+              ? options.tabBarLabel
+              : options.title !== undefined
+              ? options.title
+              : route.name;
+
+          const isFocused = state.index === index;
+
+          const onPress = () => {
+            const event = navigation.emit({
+              type: "tabPress",
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name, route.params);
+            }
+          };
+
+          const onLongPress = () => {
+            navigation.emit({
+              type: "tabLongPress",
+              target: route.key,
+            });
+          };
+
+          return (
+            <TouchableOpacity
+              key={route.key}
+              accessibilityRole="button"
+              accessibilityState={isFocused ? { selected: true } : {}}
+              accessibilityLabel={options.tabBarAccessibilityLabel}
+              testID={options.tabBarTestID}
+              onPress={onPress}
+              onLongPress={onLongPress}
+              style={{ flex: 1 }}
+            >
+              <Text style={{ color: isFocused ? "#673ab7" : "#222" }}>
+                {label}
+              </Text>
+            </TouchableOpacity>
+          );
+        }
+      )}
+    </View>
+  );
+};
+
+export default TabBar;
