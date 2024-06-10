9dc819ee15af71ae59e4b0f569529c06e780a4de
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-06-10 21:13:14 +0100

    feat: add colorlist import

--- app/explore.tsx
+++ app/explore.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import ColorList from '@/components/ColorList'
 
 const Explore = () => {
   return (
     <View>
-      <Text>Explore</Text>
+        <ColorList color="#0831b2" />
+
     </View>
   )
 }
